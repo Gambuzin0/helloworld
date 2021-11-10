e3aa13c6ff3e1f3248052f63c82cb77d86020440
Leandro <jose.leandro2002@gmail.com>
2021-11-10 11:26:58 +0000

    changed src

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,5 @@
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Thank you for using the Hello World
-                App");
+        System.out.println("Thank you for using the Hello World                                        App");
     }
 }
